ce8c381109ef431c9206ea75efdb0cc46675042b
chichchic <pch6789@naver.com>
2019-06-17 14:28:58 +0900

    float -> double, cout.precision, fixed

--- Algorithm_Problem_Solving_strategies_Book1/0617_1_6.cpp
+++ Algorithm_Problem_Solving_strategies_Book1/0617_1_6.cpp
@@ -2,28 +2,33 @@
 using namespace std;
 
 int main() {
-	float c, n, l, input[1001];
-	float result = 0.0f, ans;
+	double c, n, l, input[1001];
+	double result = 0.0f, ans;
 	cin >> c;
 	while(c){
 		cin >> n >> l;
 		
 		for(int i = 0; i < n; i++)
 			cin >> input[i];
+		
 		ans = 100.0f;
+		
 		for(int i = l; i <= n; i++)
 		{
-			for(int j = 0; j < n-i; j++)
+			for(int j = 0; j <= n-i; j++)
 			{
-				result = 0.0f;
+				result = 0.0;
 				for(int k = 0; k < i; k++)
 				{
 					result += input[j+k];
 				}
-				if(ans > result/(float)i) ans = result/(float)i;
+				result /= (double)i;
+				if(ans > result) ans = result;
 			}
 		}
 		
+		cout << fixed;
+		cout.precision(10);
 		cout << ans << endl;
 		c--;
 	}
