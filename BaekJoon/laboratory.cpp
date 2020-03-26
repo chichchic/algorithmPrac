8a31c8352318018100794aeb3e9cbc581ec6ca77
chicichich <pch6789@naver.com>
2020-03-26 18:33:36 +0900

    leetcode 200326

--- BaekJoon/laboratory.cpp
+++ BaekJoon/laboratory.cpp
@@ -1,12 +1,22 @@
 #include <iostream>
-
+#include <queue>
+#define GT greater<int>
+#define LT less<int>
 using namespace std;
 
 int main()
 {
-  freopen("input.txt", "r", stdin);
-  int a;
-  cin >> a;
-  cout << a;
+  // freopen("input.txt", "r", stdin);
+  priority_queue<int, vector<int>, LT> pq;
+  int nums[5] = {1, 2, 3, 4, 5};
+  for (auto a : nums)
+  {
+    pq.push(a);
+  }
+  while (!pq.empty())
+  {
+    cout << pq.top() << '\n';
+    pq.pop();
+  }
   return 0;
 }
