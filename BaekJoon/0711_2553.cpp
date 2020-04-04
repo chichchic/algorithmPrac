d4e8b1920e6fb43db18d862e6c77edb7f2389b2d
chicichich <pch6789@naver.com>
2020-04-04 18:10:59 +0900

    200404 BaekJoon

--- BaekJoon/0711_2553.cpp
+++ BaekJoon/0711_2553.cpp
@@ -5,16 +5,15 @@ using namespace std;
 int main()
 {
     long long n, ans = 1;
-     cin >> n;
-     for(long long i = 1; i <= n; i++)
-     {
+    cin >> n;
+    for (long long i = 1; i <= n; i++)
+    {
         ans *= i;
-        
-        while(ans % 10 == 0)
+        while (ans % 10 == 0)
         {
             ans /= 10;
         }
-        ans %= 100000;
-     }
-     cout << ans%10 << endl;
+        ans %= 10000000;
+    }
+    cout << ans % 10 << endl;
 }
