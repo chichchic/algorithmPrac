f0669f56868d679bb2be2bc055096c67af30469a
chicichich <pch6789@naver.com>
2022-03-15 21:04:13 +0900

    220315

---
+++ BaekJoon/2022/3.Mar/220315_18108.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  // freopen("../../input.txt", "r", stdin);
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  int y;
+  cin >> y;
+  cout << y - 2541 + 1998;
+
+  return 0;
+}
