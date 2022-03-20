2cee52bc270524d20295cdb12f0c6f5d8d18a08a
chicichich <pch6789@naver.com>
2022-03-20 22:24:30 +0900

    220320

---
+++ BaekJoon/2022/3.Mar/220320_11283.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+  freopen("../../input.txt", "r", stdin);
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  u16string c;
+  u16string c_stand = u"가";
+  scanf("u%s", c);
+  // cout << c[0];
+  //-c_stand[0] + 1;
+  return 0;
+}
