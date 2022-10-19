f63cc4c1393ed61d0af8e0e5e2549a23d5610d17
chichchic <chichchic6789@gmail.com>
2022-10-19 23:38:22 +0900

    refactor: 종만북 CLOCKSYNC

--- AlgorithmicProblemSolvingStrategies/CLOCKSYNC.cpp
+++ AlgorithmicProblemSolvingStrategies/CLOCKSYNC.cpp
@@ -2,11 +2,10 @@
 
 using namespace std;
 
-const int INF = 1e10;
+const int INF = 41;
 int connections[10][5] = {{0, 1, 2, -1, -1}, {3, 7, 9, 11, -1}, {4, 10, 14, 15, -1}, {0, 4, 5, 6, 7}, {6, 7, 8, 10, 12}, {0, 2, 14, 15, -1}, {3, 14, 15, -1, -1}, {4, 5, 7, 14, 15}, {1, 2, 3, 4, 5}, {3, 4, 5, 9, 13}};
 
-int cnt = 0;
-int getShortesCnt(int clock[16], int cur, int cnt)
+int getShortesCnt(int clock[16], int cur)
 {
   if (cur == 10)
   {
@@ -15,11 +14,12 @@ int getShortesCnt(int clock[16], int cur, int cnt)
       if (clock[i] != 0)
         return INF;
     }
-    return cnt;
+    return 0;
   }
   int ret = INF;
   for (int i = 0; i < 4; i++)
   {
+    ret = min(ret, i + getShortesCnt(clock, cur + 1));
     for (int j = 0; j < 5; j++)
     {
       int num = connections[cur][j];
@@ -27,19 +27,7 @@ int getShortesCnt(int clock[16], int cur, int cnt)
       {
         break;
       }
-      int prev = clock[num];
-      clock[num] = (clock[num] + i + 4) % 4;
-    }
-    ret = min(ret, getShortesCnt(clock, cur + 1, cnt + i));
-    for (int j = 0; j < 5; j++)
-    {
-      int num = connections[cur][j];
-      if (num == -1)
-      {
-        break;
-      }
-      int prev = clock[num];
-      clock[num] = (clock[num] - i + 4) % 4;
+      clock[num] = (clock[num] + 1) % 4;
     }
   }
   return ret;
@@ -62,8 +50,8 @@ int main()
       cin >> clockPos;
       clock[i] = clockPos / 3 % 4;
     }
-    int ans = getShortesCnt(clock, 0, 0);
-    cout << (ans == INF ? -1 : ans) << '\n';
+    int ans = getShortesCnt(clock, 0);
+    cout << (ans >= INF ? -1 : ans) << '\n';
   }
 
   return 0;
